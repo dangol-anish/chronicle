5bd81ff0170d468f9bbd14242afe72cdecc80026
AD <154968000+dangol-anish@users.noreply.github.com>
2024-08-07 19:06:57 +0545

    added user in sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,9 +1,18 @@
 import { createClient } from "@/utils/supabase/server";
+import { ComboboxDemo } from "./UserContent";
 
 export async function Sidebar() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <>
-      <div className="hidden md:flex flex-col w-[250px] min-w-[250px] border-r min-h-screen h-full "></div>
+      <div className="hidden md:flex flex-col w-[250px] min-w-[250px] border-r min-h-screen h-full ">
+        <ComboboxDemo user={user} />
+      </div>
     </>
   );
 }
