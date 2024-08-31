77a86b28324dee9439dee8d96b151fd123fdc384
AD <154968000+dangol-anish@users.noreply.github.com>
2024-08-31 21:23:52 +0545

    added pomodoro timer

--- components/Pomodoro/PomodoroClock.tsx
+++ components/Pomodoro/PomodoroClock.tsx
@@ -19,53 +19,69 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "../ui/use-toast";
 
 export function PomodoroClock() {
-  const [isActive, setIsActive] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
-  const [time, setTime] = useState(1500);
-  const intervalRef = useRef<number | null>(null);
-  const [newWorkTime, setNewWorkTime] = useState(isBreak ? 300 : 1500);
-  const [newBreakTime, setNewBreakTime] = useState(300);
+  const defaultWorkTime = 5; // default work time in minutes
+  const defaultBreakTime = 6; // default break time in minutes
+
+  const [workTime, setWorkTime] = useState<number>(defaultWorkTime * 60); // default work time in seconds
+  const [breakTime, setBreakTime] = useState<number>(defaultBreakTime * 60); // default break time in seconds
+  const [time, setTime] = useState<number>(workTime);
+  const [currentMode, setCurrentMode] = useState<"work" | "break">("work");
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const { toast } = useToast();
 
   useEffect(() => {
-    if (isActive) {
-      intervalRef.current = window.setInterval(() => {
-        setTime((prevTime) => {
-          if (prevTime <= 0) {
-            clearInterval(intervalRef.current!);
-            setIsBreak((prev) => !prev);
-            setIsActive(false);
-            return isBreak ? newBreakTime : newWorkTime;
-          }
-          return prevTime - 1;
-        });
+    if (isActive && time > 0) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
       }, 1000);
-    } else if (!isActive && intervalRef.current !== null) {
-      clearInterval(intervalRef.current!);
+    } else if (time === 0) {
+      if (currentMode === "work") {
+        setCurrentMode("break");
+        setTime(breakTime);
+      } else {
+        setCurrentMode("work");
+        setTime(workTime);
+      }
+      setIsActive(false); // Stop the timer after switching modes
     }
 
     return () => clearInterval(intervalRef.current!);
-  }, [isActive, isBreak, newWorkTime, newBreakTime]);
+  }, [isActive, time, currentMode, breakTime, workTime]);
 
-  const handleStartPause = () => {
-    setIsActive(!isActive);
-  };
-
-  const handleReset = () => {
-    setIsActive(false);
-    setTime(isBreak ? newBreakTime : newWorkTime);
+  const resetTimer = () => {
     clearInterval(intervalRef.current!);
-  };
-
-  const handleToggle = (isBreakTimer: boolean) => {
-    setIsBreak(isBreakTimer);
-    setTime(isBreakTimer ? newBreakTime : newWorkTime);
     setIsActive(false);
-    clearInterval(intervalRef.current!);
+    setTime(currentMode === "work" ? workTime : breakTime);
   };
 
-  const formatTime = (seconds: number) => {
+  const handleSetTime = (newWorkTime: number, newBreakTime: number) => {
+    if (newWorkTime > 60 || newBreakTime > 60) {
+      toast({
+        description: "Work and break times cannot exceed 60 minutes",
+        title: "Error",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    const workTimeInSeconds = newWorkTime * 60;
+    const breakTimeInSeconds = newBreakTime * 60;
+
+    setWorkTime(workTimeInSeconds);
+    setBreakTime(breakTimeInSeconds);
+
+    if (currentMode === "work") {
+      setTime(workTimeInSeconds);
+    } else {
+      setTime(breakTimeInSeconds);
+    }
+  };
+
+  const formatTime = (seconds: number): string => {
     const minutes = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(
@@ -74,98 +90,97 @@ export function PomodoroClock() {
     )}`;
   };
 
-  const handleSaveChanges = () => {
-    setTime(isBreak ? newBreakTime : newWorkTime);
-  };
-
   return (
     <Card className="w-[500px] h-[500px] flex flex-col">
+      {/* header */}
       <CardHeader>
         <div className="flex flex-col justify-center w-full items-center gap-6">
           <CardTitle className="flex gap-2 items-center">
-            <p>Pomodoro Timer</p>
+            <p>Timer</p>
           </CardTitle>
           <div className="flex justify-center gap-6">
             <Button
-              onClick={() => handleToggle(false)}
-              variant={!isBreak ? "default" : "outline"}
+              variant={currentMode === "work" ? "default" : "outline"}
+              onClick={() => {
+                if (currentMode !== "work") {
+                  setCurrentMode("work");
+                  setIsActive(false); // Stop the timer when switching modes
+                  setTime(workTime);
+                }
+              }}
             >
               Work Time
             </Button>
             <Button
-              onClick={() => handleToggle(true)}
-              variant={isBreak ? "default" : "outline"}
+              variant={currentMode === "break" ? "default" : "outline"}
+              onClick={() => {
+                if (currentMode !== "break") {
+                  setCurrentMode("break");
+                  setIsActive(false); // Stop the timer when switching modes
+                  setTime(breakTime);
+                }
+              }}
             >
               Break Time
             </Button>
           </div>
         </div>
       </CardHeader>
+      {/* clock */}
       <CardContent className="flex-grow flex flex-col gap-10 justify-center items-center text-9xl">
         <p>{formatTime(time)}</p>
       </CardContent>
+      {/* footer */}
       <CardFooter className="flex justify-center gap-6">
-        <Button onClick={handleReset}>Reset</Button>
-        <div>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="outline">Set Time</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Set New Time</DialogTitle>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="work-time" className="text-right">
-                    Work Time
-                  </Label>
-                  <Input
-                    id="work-time"
-                    type="number"
-                    className="col-span-3"
-                    value={newWorkTime / 60}
-                    onChange={(e) => {
-                      const value = parseInt(e.target.value, 10);
-                      setNewWorkTime(
-                        isNaN(value) || value < 0 || value > 100
-                          ? newWorkTime
-                          : value * 60
-                      );
-                    }}
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="break-time" className="text-right">
-                    Break Time
-                  </Label>
-                  <Input
-                    id="break-time"
-                    type="number"
-                    className="col-span-3"
-                    value={newBreakTime / 60}
-                    onChange={(e) => {
-                      const value = parseInt(e.target.value, 10);
-                      setNewBreakTime(
-                        isNaN(value) || value < 0 || value > 100
-                          ? newBreakTime
-                          : value * 60
-                      );
-                    }}
-                  />
-                </div>
+        <Button onClick={resetTimer}>Reset</Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Set Time</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Set New Time</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="work-time" className="text-right">
+                  Work Time
+                </Label>
+                <Input
+                  id="work-time"
+                  type="number"
+                  defaultValue={defaultWorkTime}
+                  className="col-span-3"
+                  onChange={(e) =>
+                    handleSetTime(Number(e.target.value), breakTime / 60)
+                  }
+                />
               </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="submit" onClick={handleSaveChanges}>
-                    Save
-                  </Button>
-                </DialogClose>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </div>
-        <Button className="" onClick={handleStartPause}>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="break-time" className="text-right">
+                  Break Time
+                </Label>
+                <Input
+                  id="break-time"
+                  type="number"
+                  defaultValue={defaultBreakTime}
+                  className="col-span-3"
+                  onChange={(e) =>
+                    handleSetTime(workTime / 60, Number(e.target.value))
+                  }
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="submit">Save</Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Button onClick={() => setIsActive(!isActive)}>
           {isActive ? "Pause" : "Start"}
         </Button>
       </CardFooter>
