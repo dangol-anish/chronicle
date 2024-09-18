4d40f8463515308b035702e3ef69dbf647aa2e23
AD <154968000+dangol-anish@users.noreply.github.com>
2024-09-18 23:28:27 +0545

    deployment test

--- components/Sidebar/SidebarName.tsx
+++ components/Sidebar/SidebarName.tsx
@@ -1,10 +1,17 @@
-import { Kanban } from "lucide-react";
+import Logo from "../../public/chronicles-light.svg";
+import Image from "next/image";
 
 export function SidebarName() {
   return (
     <>
       <div className="flex items-center lg:px-2">
-        <Kanban size={35} />
+        <Image
+          src={Logo}
+          alt="logo"
+          className="dark:invert"
+          width={25}
+          height={25}
+        />
         <p className="text-[32px] hidden lg:flex ">Chronicle</p>
       </div>
     </>
